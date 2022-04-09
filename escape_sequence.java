f0e920210075d1e22fbe4652f88e1ef4d416aef1
Tushar Dahiya <tusharxoxoxo@gmail.com>
2022-04-09 23:06:47 +0530

    Delete escape_sequence.java

--- escape_sequence.java
+++
@@ -1,8 +0,0 @@
-public class escape_sequence{
-  public static void main(String[] args){
-    System.out.println("what is my \"name\"");
-    System.out.println("\\windows\\");
-    System.out.println("what\nthe\nfuck");
-    System.out.println("what\tthe\tfuck");
-  }
-}
