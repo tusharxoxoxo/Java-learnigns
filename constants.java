38fe19f3c9cb622260f2b26abb2d9daf81855701
Tushar Dahiya <tusharxoxoxo@gmail.com>
2022-04-09 22:58:58 +0530

    Add files via upload

---
+++ constants.java
@@ -0,0 +1,6 @@
+public class constants{
+  public static void main(String[] args){
+    final float PI = 3.14F;
+            System.out.println(PI);
+  }
+}
---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable{
+      public static void main(String[] args){
+          byte age = 20;
+          int viewCount = 3434_3434;
+          long actualViewCount = 222_23434_234L;
+          float cash = 10.99F;
+          boolean isTrue = false;
+          System.out.println(age + " " + viewCount + " " + actualViewCount + " " + cash + " " + isTrue);
+
+  }
+}
